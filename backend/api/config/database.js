3a239f5241f4cb5ed44ff7441bf7ecb318a7102a
Mochamad Afif Risdiyanto <53353746+afdi18@users.noreply.github.com>
2022-12-08 08:54:40 +0700

    Update database.js

--- backend/api/config/database.js
+++ backend/api/config/database.js
@@ -1,27 +1,4 @@
-// const {buatPool}=require("mysql2");
 
-// const pool = buatPool({
-//     host:process.env.DB_HOST,
-//     port:process.env.DB_PORT,
-//     user:process.env.DB_USER,
-//     password:process.env.DB_PASS,
-//     database:process.env.DB_NAME,
-//     connectionLimit:10
-// });
-
-// module.exports = {
-//     HOST:process.env.DB_HOST,
-//     USER:process.env.DB_USER,
-//     PASSWORD:process.env.DB_PASS,
-//     DB:process.env.DB_NAME,
-//     dialect:"mysql",
-//     pool:{
-//         max: 5,
-//         min:0,
-//         acquire: 30000,
-//         idle:10000
-//     }
-// };
 
 import {Sequelize} from "sequelize";
 // const user = process.env.DB_USER;
